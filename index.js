9b71c33e7cf22dc344e2955f1db38fe82ca41f7f
ramsey900 <ramsey.anyona@student.moringaschool.com>
2025-06-29 18:55:42 +0300

    context lab

--- index.js
+++ index.js
@@ -1,5 +1,65 @@
 /* Your Code Here */
 
+function createEmployeeRecord(array) {
+  return {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+}
+
+function createEmployeeRecords(arrays) {
+  return arrays.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(dateStamp) {
+  const [date, hour] = dateStamp.split(' ')
+  this.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour),
+    date: date
+  })
+  return this
+}
+
+function createTimeOutEvent(dateStamp) {
+  const [date, hour] = dateStamp.split(' ')
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour),
+    date: date
+  })
+  return this
+}
+
+function hoursWorkedOnDate(date) {
+  const timeIn = this.timeInEvents.find(event => event.date === date)
+  const timeOut = this.timeOutEvents.find(event => event.date === date)
+  
+  if (timeIn && timeOut) {
+    return (timeOut.hour - timeIn.hour) / 100
+  }
+  return 0
+}
+
+function wagesEarnedOnDate(date) {
+  const hours = hoursWorkedOnDate.call(this, date)
+  return hours * this.payPerHour
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+  return srcArray.find(employee => employee.firstName === firstName)
+}
+
+function calculatePayroll(employeeRecords) {
+  return employeeRecords.reduce((total, employee) => {
+    return total + allWagesFor.call(employee)
+  }, 0)
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -18,6 +78,5 @@ const allWagesFor = function () {
         return memo + wagesEarnedOnDate.call(this, d)
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
-}
-
+    return payable;
+}   
